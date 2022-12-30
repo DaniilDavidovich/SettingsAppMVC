34a4301e59828c301de6ccf35783ade91b6b61cd
Daniil Davidovich <dd.jopgm@gmail.com>
2022-12-30 07:32:30 +0300

    add custom Table View Cell

--- Home Work 13/Sources/Model.swift
+++ Home Work 13/Sources/Model.swift
@@ -14,37 +14,37 @@ enum Blocks  {
     case appStoreAndWallet
 }
 
-struct Cell {
+struct Cells: Hashable{
     var name: String
-    var image: UIImage
+    var icon: UIImage
     var block: Blocks
 }
 
-extension Cell {
+extension Cells {
     
-    static var Cells: [[Cell]] = [
+    static var Cells: [[Cells]] = [
         // Cellular And Network
-        [Cell(name: "Airplane Mode", image: UIImage(systemName: "airplane.circle.fill")!, block: .cellularAndNetwork)],
-        [Cell(name: "Wi-Fi", image: UIImage(systemName: "wifi.square.fill")!, block: .cellularAndNetwork)],
-        [Cell(name: "Bluetooch", image: UIImage(systemName: "wifi.square.fill")!, block: .cellularAndNetwork)],
-        [Cell(name: "Mobile Data", image: UIImage(systemName: "antenna.radiowaves.left.and.right")!, block: .cellularAndNetwork)],
-        [Cell(name: "Personal Hotspot", image: UIImage(systemName: "personalhotspot")!, block: .cellularAndNetwork)],
-        [Cell(name: "VPN", image: UIImage(named: "VPN")!, block: .cellularAndNetwork)],
+        [Cells(name: "Airplane Mode", icon: UIImage(systemName: "airplane.circle.fill")!, block: .cellularAndNetwork)],
+        [Cells(name: "Wi-Fi", icon: UIImage(systemName: "wifi.square.fill")!, block: .cellularAndNetwork)],
+        [Cells(name: "Bluetooch", icon: UIImage(systemName: "wifi.square.fill")!, block: .cellularAndNetwork)],
+        [Cells(name: "Mobile Data", icon: UIImage(systemName: "antenna.radiowaves.left.and.right")!, block: .cellularAndNetwork)],
+        [Cells(name: "Personal Hotspot", icon: UIImage(systemName: "personalhotspot")!, block: .cellularAndNetwork)],
+        [Cells(name: "VPN", icon: UIImage(named: "VPN")!, block: .cellularAndNetwork)],
         // Notification
-        [Cell(name: "Notification", image: UIImage(systemName: "wifi.square.fill")!, block: .notification)],
-        [Cell(name: "Sounds & Haptics", image: UIImage(systemName: "wifi.square.fill")!, block: .notification)],
-        [Cell(name: "Focus", image: UIImage(systemName: "wifi.square.fill")!, block: .notification)],
-        [Cell(name: "Screen Time", image: UIImage(systemName: "wifi.square.fill")!, block: .notification)],
+        [Cells(name: "Notification", icon: UIImage(systemName: "wifi.square.fill")!, block: .notification)],
+        [Cells(name: "Sounds & Haptics", icon: UIImage(systemName: "wifi.square.fill")!, block: .notification)],
+        [Cells(name: "Focus", icon: UIImage(systemName: "wifi.square.fill")!, block: .notification)],
+        [Cells(name: "Screen Time", icon: UIImage(systemName: "wifi.square.fill")!, block: .notification)],
         // General
-        [Cell(name: "General", image: UIImage(systemName: "airplane.circle.fill")!, block: .general)],
-        [Cell(name: "Control Centre", image: UIImage(systemName: "wifi.square.fill")!, block: .general)],
-        [Cell(name: "Display & Brightness", image: UIImage(systemName: "wifi.square.fill")!, block: .general)],
-        [Cell(name: "Home Screen", image: UIImage(systemName: "antenna.radiowaves.left.and.right")!, block: .general)],
-        [Cell(name: "Accessibility", image: UIImage(systemName: "personalhotspot")!, block: .general)],
-        [Cell(name: "Walpapper", image: UIImage(named: "VPN")!, block: .cellularAndNetwork)],
+        [Cells(name: "General", icon: UIImage(systemName: "airplane.circle.fill")!, block: .general)],
+        [Cells(name: "Control Centre", icon: UIImage(systemName: "wifi.square.fill")!, block: .general)],
+        [Cells(name: "Display & Brightness", icon: UIImage(systemName: "wifi.square.fill")!, block: .general)],
+        [Cells(name: "Home Screen", icon: UIImage(systemName: "antenna.radiowaves.left.and.right")!, block: .general)],
+        [Cells(name: "Accessibility", icon: UIImage(systemName: "personalhotspot")!, block: .general)],
+        [Cells(name: "Walpapper", icon: UIImage(named: "VPN")!, block: .cellularAndNetwork)],
         // AppStoreAndWallet
-        [Cell(name: "App Store", image: UIImage(systemName: "wifi.square.fill")!, block: .appStoreAndWallet)],
-        [Cell(name: "Wallet & Apple Pay", image: UIImage(systemName: "wifi.square.fill")!, block: .appStoreAndWallet)],
+        [Cells(name: "App Store", icon: UIImage(systemName: "wifi.square.fill")!, block: .appStoreAndWallet)],
+        [Cells(name: "Wallet & Apple Pay", icon: UIImage(systemName: "wifi.square.fill")!, block: .appStoreAndWallet)],
         
     ]
 }
