92649a38c147be745c1af99c0f18d905a881a744
Daniil Davidovich <dd.jopgm@gmail.com>
2022-12-30 05:48:21 +0300

    Initial Commit

---
+++ Home Work 13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home Work 13
+//
+//  Created by Daniil Davidovich on 30.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
