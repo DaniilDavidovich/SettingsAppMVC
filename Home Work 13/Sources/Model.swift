54490701076e9bc74638125ac44df6755c43c2c7
Daniil Davidovich <dd.jopgm@gmail.com>
2023-01-04 21:56:07 +0300

    add nil coalescing to uiimage

--- Home Work 13/Sources/Model.swift
+++ Home Work 13/Sources/Model.swift
@@ -27,26 +27,26 @@ extension Cell {
     // NOW ALWAYS IN ARRAY(ONE ARRAY FOR ONE SECTION)
     static var cells: [[Cell]] = [
         // Cellular And Network
-        [Cell(name: "Airplane Mode", icon: UIImage(named: "airplanemode")!, block: .cellularAndNetwork, toggle: true, text: nil),
-         Cell(name: "Wi-Fi", icon: UIImage(named: "wifi")!, block: .cellularAndNetwork, toggle: true),
-         Cell(name: "Bluetooth", icon: UIImage(named: "bluetooth")!, block: .cellularAndNetwork, toggle: true, text: nil),
-         Cell(name: "Mobile Data", icon: UIImage(named: "cellular")!, block: .cellularAndNetwork, toggle: true, text: nil),
-         Cell(name: "Personal Hotspot", icon: UIImage(named: "hotspot")!, block: .cellularAndNetwork, toggle: true, text: nil),
-         Cell(name: "VPN", icon: UIImage(named: "vpn")!, block: .cellularAndNetwork, toggle: true, text: nil)],
+        [Cell(name: "Airplane Mode", icon: UIImage(named: "airplanemode") ?? UIImage(systemName: "trash")!, block: .cellularAndNetwork, toggle: true, text: nil),
+         Cell(name: "Wi-Fi", icon: UIImage(named: "wifi") ?? UIImage(systemName: "trash")!, block: .cellularAndNetwork, toggle: true),
+         Cell(name: "Bluetooth", icon: UIImage(named: "bluetooth") ?? UIImage(systemName: "trash")!, block: .cellularAndNetwork, toggle: true, text: nil),
+         Cell(name: "Mobile Data", icon: UIImage(named: "cellular") ?? UIImage(systemName: "trash")!, block: .cellularAndNetwork, toggle: true, text: nil),
+         Cell(name: "Personal Hotspot", icon: UIImage(named: "hotspot") ?? UIImage(systemName: "trash")!, block: .cellularAndNetwork, toggle: true, text: nil),
+         Cell(name: "VPN", icon: UIImage(named: "vpn") ?? UIImage(systemName: "trash")!, block: .cellularAndNetwork, toggle: true, text: nil)],
         // Notification
-        [Cell(name: "Notification", icon: UIImage(named: "notification")!, block: .notification, toggle: nil, text: "Add notification"),
-         Cell(name: "Sounds & Haptics", icon: UIImage(named: "sound")!, block: .notification, toggle: nil, text: nil),
-         Cell(name: "Focus", icon: UIImage(named: "focus")!, block: .notification, toggle: nil, text: nil),
-         Cell(name: "Screen Time", icon: UIImage(named: "screentime")!, block: .notification, toggle: nil, text: nil)],
+        [Cell(name: "Notification", icon: UIImage(named: "notification") ?? UIImage(systemName: "trash")!, block: .notification, toggle: nil, text: "Add notification"),
+         Cell(name: "Sounds & Haptics", icon: UIImage(named: "sound") ?? UIImage(systemName: "trash")!, block: .notification, toggle: nil, text: nil),
+         Cell(name: "Focus", icon: UIImage(named: "focus") ?? UIImage(systemName: "trash")!, block: .notification, toggle: nil, text: nil),
+         Cell(name: "Screen Time", icon: UIImage(named: "screentime") ?? UIImage(systemName: "trash")!, block: .notification, toggle: nil, text: nil)],
         // General
-        [Cell(name: "General", icon: UIImage(named: "settings")!, block: .general, toggle: nil, text: nil),
-         Cell(name: "Face ID & Passcode", icon: UIImage(named: "faceid")!, block: .general, toggle: nil, text: nil),
-         Cell(name: "Privacy", icon: UIImage(named: "privacy")!, block: .general, toggle: nil, text: nil),
-         Cell(name: "Battary", icon: UIImage(named: "battery")!, block: .general, toggle: nil, text: nil),
-         Cell(name: "Emergency SOS", icon: UIImage(named: "sos")!, block: .general, toggle: nil, text: nil),
-         Cell(name: "Wallpapper", icon: UIImage(named: "wallpaper")!, block: .general, toggle: nil, text: "Add wallpaper")],
+        [Cell(name: "General", icon: UIImage(named: "settings") ?? UIImage(systemName: "trash")!, block: .general, toggle: nil, text: nil),
+         Cell(name: "Face ID & Passcode", icon: UIImage(named: "faceid") ?? UIImage(systemName: "trash")!, block: .general, toggle: nil, text: nil),
+         Cell(name: "Privacy", icon: UIImage(named: "privacy") ?? UIImage(systemName: "trash")!, block: .general, toggle: nil, text: nil),
+         Cell(name: "Battary", icon: UIImage(named: "battery") ?? UIImage(systemName: "trash")!, block: .general, toggle: nil, text: nil),
+         Cell(name: "Emergency SOS", icon: UIImage(named: "sos") ?? UIImage(systemName: "trash")!, block: .general, toggle: nil, text: nil),
+         Cell(name: "Wallpapper", icon: UIImage(named: "wallpaper") ?? UIImage(systemName: "trash")!, block: .general, toggle: nil, text: "Add wallpaper")],
         // AppStoreAndWallet
-        [Cell(name: "App Store", icon: UIImage(named: "appstore")!, block: .appStoreAndWallet, toggle: nil, text: nil),
-         Cell(name: "Wallet & Apple Pay", icon: UIImage(named: "wallet")!, block: .appStoreAndWallet, toggle: nil, text: nil)],
+        [Cell(name: "App Store", icon: UIImage(named: "appstore") ?? UIImage(systemName: "trash")!, block: .appStoreAndWallet, toggle: nil, text: nil),
+         Cell(name: "Wallet & Apple Pay", icon: UIImage(named: "wallet") ?? UIImage(systemName: "trash")!, block: .appStoreAndWallet, toggle: nil, text: nil)],
     ]
 }
