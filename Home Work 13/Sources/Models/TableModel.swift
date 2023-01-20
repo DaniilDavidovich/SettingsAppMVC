d0fe8e8a5096e133875b26c98cdf4318e7020de7
Daniil Davidovich <dd.jopgm@gmail.com>
2023-01-20 05:30:33 +0300

    small code refactoring

--- Home Work 13/Sources/Models/TableModel.swift
+++ Home Work 13/Sources/Models/TableModel.swift
@@ -33,37 +33,3 @@ import Foundation
             Table(name: "Wallet & Apple Pay", icon: "wallet")]
             ]
     }
-
-//extension Cell {
-//
-//    //ONE ARRAY FOR ONE SECTION
-//    static var cells: [[Cell]] = [
-//
-////         Cellular And Network
-//        [Cell(name: "Airplane Mode", icon: "airplanemode", block: .cellularAndNetwork, toggle: true, text: nil),
-//         Cell(name: "Wi-Fi", icon: "wifi", block: .cellularAndNetwork, toggle: true),
-//         Cell(name: "Bluetooth", icon: "bluetooth", block: .cellularAndNetwork, toggle: true, text: nil),
-//         Cell(name: "Mobile Data", icon: "cellular", block: .cellularAndNetwork, toggle: true, text: nil),
-//         Cell(name: "Personal Hotspot", icon: "hotspot" , block: .cellularAndNetwork, toggle: true, text: nil),
-//         Cell(name: "VPN", icon: "vpn", block: .cellularAndNetwork, toggle: true, text: nil)],
-//
-//        // Notification
-//        [Cell(name: "Notification", icon:
-//                 "notification", block: .notification, toggle: nil, text: "Add notification"),
-//         Cell(name: "Sounds & Haptics", icon: "sound", block: .notification, toggle: nil, text: nil),
-//         Cell(name: "Focus", icon: "focus", block: .notification, toggle: nil, text: nil),
-//         Cell(name: "Screen Time", icon: "screentime", block: .notification, toggle: nil, text: nil)],
-//
-//        // General
-//        [Cell(name: "General", icon: "settings", block: .general, toggle: nil, text: nil),
-//         Cell(name: "Face ID & Passcode", icon: "faceid", block: .general, toggle: nil, text: nil),
-//         Cell(name: "Privacy", icon: "privacy", block: .general, toggle: nil, text: nil),
-//         Cell(name: "Battary", icon:  "battery", block: .general, toggle: nil, text: nil),
-//         Cell(name: "Emergency SOS", icon: "sos", block: .general, toggle: nil, text: nil),
-//         Cell(name: "Wallpapper", icon: "wallpaper", block: .general, toggle: nil, text: "Add wallpaper")],
-//
-//        // AppStoreAndWallet
-//        [Cell(name: "App Store", icon: "appstore", block: .appStoreAndWallet, toggle: nil, text: nil),
-//         Cell(name: "Wallet & Apple Pay", icon: "wallet", block: .appStoreAndWallet, toggle: nil, text: nil)],
-//    ]
-//}
